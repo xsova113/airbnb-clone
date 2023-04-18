900d45de051adbee60c2f23ce20099b2d046af74
George <xsova113@gmail.com>
2023-04-17 19:17:29 -0700

    Add locationvalue in index.d and prismadb

--- app/action/getListings.ts
+++ app/action/getListings.ts
@@ -87,7 +87,7 @@ export default async function getListings(
         const safeListings = listings.map(listing => ({
             ...listing,
             createdAt: listing.createdAt.toISOString()
-        }))
+        }));
 
         return safeListings;
     } catch (error: any) {
--- app/types/index.ts
+++ app/types/index.ts
@@ -1,11 +1,8 @@
 import { User, Listing, Reservation } from "@prisma/client"
 
-export type SafeListing = Omit<
-    Listing,
-    "createdAt"
-> & {
+export type SafeListing = Omit<Listing, "createdAt"> & {
     createdAt: string;
-}
+  };
 
 export type SafeReservations = Omit<
     Reservation,
